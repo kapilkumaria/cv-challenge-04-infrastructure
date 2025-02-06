960fbc4feca13078b5599617a28dcf87952a5a8a
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2025-02-05 22:18:04 -0700

    add aws-eks-demo-cluster-without-modules

--- terraform-new/infrastructure/terraform/prod/backend.tf
+++
@@ -1,7 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "eks-statefile-bucker1234"
-#     key    = "prod/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
--- terraform-new/infrastructure/terraform/prod/kubeconfig.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "update_kubeconfig" {
-  provisioner "local-exec" {
-    command = "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name}"
-  }
-
-  triggers = {
-    cluster_name = module.eks.cluster_name
-  }
-}
